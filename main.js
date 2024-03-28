3962027ea83905f6a329e6bbe8c1f933786dc4ba
unknown <fa51214kh@gmail.com>
2024-03-29 00:35:20 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+const randomNumber = 42;
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: chalk.yellow("Please Guess Any Number:"),
+    }
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log(chalk.bgBlueBright.bold("Congratulations! You guessed correctly."));
+}
+else {
+    console.log(chalk.bgMagentaBright.bold("Oops! Try again."));
+}
